cb0f107d884c474655c4a477024af114f99119ea
BenjaminLoch <bloch@hof-university.de>
2025-11-08 18:29:41 +0100

    Safestate

--- Assets/Scripts/PlayerDash.cs
+++ Assets/Scripts/PlayerDash.cs
@@ -1,14 +1,24 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerDash : MonoBehaviour
 {
     [SerializeField] private Transform mousePos;
-    [SerializeField] private Transform objPos;
-    [SerializeField] private Rigidbody rb;
-    [SerializeField] private float dashSpeed;
-    [SerializeField] private float dashTime;
+    private Transform objPos;
+    private Rigidbody thisRB;
+    private GameObject thisGameObj;
+    [SerializeField] private float dashCount = 7f;
+    [SerializeField] private float dashSpeed = 45f;
+    [SerializeField] private float dashTime = 0.15f;
 
+    private void Start()
+    {
+        thisRB = GetComponent<Rigidbody>();
+        thisGameObj = gameObject;
+        Transform child = transform.Find("ObjRefPoint");
+        objPos = child;
+    }
     private void Update()
     {
         Vector3 guckRichtung = (mousePos.position - objPos.position).normalized;
@@ -19,12 +29,23 @@ public class PlayerDash : MonoBehaviour
         }
     }
 
+    private void Die()
+    {
+        Debug.Log("Spieler ist tot!");
+        Destroy(thisGameObj); //Spieler wird zerstoert
+    }
     private System.Collections.IEnumerator Dash()
     {
         Vector3 moveVektor = (mousePos.position - objPos.position).normalized;
-        rb.linearVelocity = dashSpeed * moveVektor;
+        thisRB.linearVelocity = dashSpeed * moveVektor;
         yield return new WaitForSeconds(dashTime);
 
-        rb.linearVelocity = Vector3.zero;
+        thisRB.linearVelocity = Vector3.zero;
+        dashCount--;
+        Debug.Log("Dash uebrig: " + dashCount); //Aktuellen dash Wert in Konsole
+        if (dashCount == 0)
+        {
+            Die(); //Spieler stirbt
+        }
     }
 }
